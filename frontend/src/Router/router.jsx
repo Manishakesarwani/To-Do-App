403eac4633f90d4f87913fde82db41f056eb7726
ManishaKesarwani <manishakesarwani9354@gmail.com>
2025-12-18 21:06:23 +0530

    updated router file error

--- frontend/src/Router/router.jsx
+++ frontend/src/Router/router.jsx
@@ -1,6 +1,6 @@
 import App from "../App";
 import CreateToDo from "../components/CreateToDo";
-import GetAllTodo from "../components/GetAllTodo";
+import GetAllToDo from "../components/GetAllTodo";
 import Home from "../pages/Home";
 import { createBrowserRouter } from "react-router-dom";
 import UpdateToDo from "../components/UpdateToDo";
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
     children: [
       {index: true, element: <Home />},
       {path:"create", element:<CreateToDo />},
-      {path:"getAll", element:<GetAllTodo />},
+      {path:"getAll", element:<GetAllToDo />},
       {path:"update/:id", element:<UpdateToDo />},
       {path:"delete/:id", element:<DeleteToDo />},
       {path:"incompleteToDo", element:<GetIncompleteToDo />},
